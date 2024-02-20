f05312f2d5bb379078d09b9a32702148ef449e36
Luis <siul210704@gmail.com>
2024-02-20 10:25:05 -0400

    format code

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,20 @@
-import express from "express";
-import matenimiento_router from "./routes/mantenimiento/index.js";
-const app = express();
+import express from 'express'
+import matenimientoRouter from './routes/mantenimiento/index.js'
+const app = express()
 
-
-//Router api 
-const ApiRouter = express.Router();
-
+// Router api
+const ApiRouter = express.Router()
 
 // add the mantenimiento_router to the api router
-ApiRouter.use("/mantenimiento",matenimiento_router);
-
-
-
-
-
-
-
+ApiRouter.use('/mantenimiento', matenimientoRouter)
 
 // add the api router to the app
-app.use("/api",ApiRouter);
+app.use('/api', ApiRouter)
 
 // add the frontend to the app
-app.use(express.static("frontend"));
-
-
+app.use(express.static('frontend'))
 
 //  start the server
-app.listen(3000,()=>{
-    console.log("Server is running on  http://localhost:3000/");
+app.listen(3000, () => {
+  console.log('Server is running on  http://localhost:3000/')
 })
--- backend/routes/mantenimiento/index.js
+++ backend/routes/mantenimiento/index.js
@@ -1,13 +1,11 @@
-import express from "express";
+import express from 'express'
 
 // create a new router to handle the mantenimiento routes
 const Router = express.Router()
 
-
-Router.get("/",(req,res)=>{
-    res.send("Mantenimiento");
+Router.get('/', (req, res) => {
+  res.json({ message: 'this is not implemented yet, please come back later' })
 })
 
-
 // export the router
-export default Router;
+export default Router
