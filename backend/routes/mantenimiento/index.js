a7335c3d113570c8e5a4404ed34b85abf687fa54
Luis <siul210704@gmail.com>
2024-02-20 18:04:06 -0400

    change package json

--- backend/routes/mantenimiento/index.js
+++ backend/routes/mantenimiento/index.js
@@ -1,10 +1,19 @@
 import express from 'express'
+import { createClient } from '@libsql/client'
+
+const client = createClient({
+  authToken: process.env.DB_MAN_TOKEN,
+  url: process.env.DB_MAN_URL
+})
 
 // create a new router to handle the mantenimiento routes
 const Router = express.Router()
 
-Router.get('/', (req, res) => {
-  res.json({ message: 'this is not implemented yet, please come back later' })
+Router.get('/', async (req, res) => {
+// get machines
+  const { rows: machines } = await client.execute('SELECT * FROM MACHINE')
+
+  res.json({ message: 'this is not implemented yet, please come back later', machines })
 })
 
 // export the router
