5f6385e525495f60d25efa5b4a8d48fb83b6660d
Luis <siul210704@gmail.com>
2024-02-21 23:16:31 -0400

    update routes and create model for machine and maintenance

--- backend/routes/mantenimiento/index.js
+++ backend/routes/mantenimiento/index.js
@@ -1,19 +1,110 @@
 import express from 'express'
-import { createClient } from '@libsql/client'
-
-const client = createClient({
-  authToken: process.env.DB_MAN_TOKEN,
-  url: process.env.DB_MAN_URL
-})
-
+import client from './model.js'
 // create a new router to handle the mantenimiento routes
 const Router = express.Router()
 
+// get the machines and the maintenance from the database
 Router.get('/', async (req, res) => {
-// get machines
-  const { rows: machines } = await client.execute('SELECT * FROM MACHINE')
+// get the machines and the maintenance from the database
 
-  res.json({ message: 'this is not implemented yet, please come back later', machines })
+  // make a query to the database update the machines  state in base of the maintenance date
+  await client.execute({ sql: 'UPDATE MACHINE SET state = ? , availability = 0  WHERE date((select dateMaintenance from  MAINTENANCE WHERE MACHINE.id =  MAINTENANCE.machineId)) <= date() AND date((select dateAvailability from  MAINTENANCE WHERE MACHINE.id =  MAINTENANCE.machineId)) >= date();', args: ['mantenimiento'] })
+  await client.execute({ sql: 'DELETE FROM MAINTENANCE WHERE date(dateAvailability) <= date()' })
+
+  const { rows: machines } = await client.execute('SELECT MACHINE.*, MAINTENANCE.type as typeMaintenance ,MAINTENANCE.dateMaintenance, MAINTENANCE.dateAvailability  FROM MACHINE LEFT JOIN MAINTENANCE ON MACHINE.id = MAINTENANCE.machineId')
+
+  res.json({ machines })
+})
+
+// create a new maintenance
+Router.post('/', async (req, res) => {
+  // get the data from the request
+  const { type, dateMaintenance, dateAvailability, machineId } = req.body
+
+  // insert the maintenance into the database
+  try {
+    await client.execute({ sql: 'INSERT INTO MAINTENANCE (machineId, type, dateMaintenance, dateAvailability) VALUES (?, ?, ?, ?)', args: [machineId, type, dateMaintenance, dateAvailability] })
+    res.status(201).end()
+  } catch (error) {
+    console.error(error)
+    res.status(500).end()
+  }
+})
+
+// update the maintenance
+Router.patch('/:id', async (req, res) => {
+  // get the id from the request
+  const { id } = req.params
+  // get the data from the request
+  const { dateMaintenance = null, dateAvailability = null } = req.body
+  // update the maintenance in the database
+  try {
+    await client.execute({ sql: 'UPDATE MAINTENANCE SET dateMaintenance = ?, dateAvailability = ? WHERE id = ?', args: [dateMaintenance, dateAvailability, id] })
+    res.status(204).end()
+  } catch (error) {
+    console.error(error)
+    res.status(500).end()
+  }
+})
+
+// delete the maintenance
+Router.delete('/:id', async (req, res) => {
+  // get the id from the request
+  const { id } = req.params
+  // delete the maintenance from the database
+  try {
+    await client.execute({ sql: 'DELETE FROM MAINTENANCE WHERE id = ?', args: [id] })
+    res.status(200).end()
+  } catch (error) {
+    console.error(error)
+    res.status(500).end()
+  }
+})
+
+Router.patch('/machine/:id', async (req, res) => {
+  // get the id from the request
+  const { id } = req.params
+  // get the data from the request
+  const { state } = req.body
+  // update the machine in the database
+  try {
+    await client.execute({ sql: 'UPDATE MACHINE SET state = ? WHERE id = ?', args: [state, id] })
+    res.status(204).end()
+  } catch (error) {
+    console.error(error)
+    res.status(500).end()
+  }
+})
+
+Router.post('/machine', async (req, res) => {
+  // get the data from the request
+  const { type, line } = req.body
+  // insert the machine into the database
+  try {
+    await client.execute({ sql: 'INSERT INTO MACHINE (type, state, availability, line) VALUES (?, ?, ?, ?)', args: [type, 'disponible', 1, line] })
+    res.status(201).end()
+  } catch (error) {
+    console.error(error)
+    res.status(500).end()
+  }
+})
+
+Router.delete('/machine/:id', async (req, res) => {
+  // get the id from the request
+  const { id } = req.params
+  // delete the machine from the database
+  try {
+    await client.execute({ sql: 'DELETE FROM MACHINE WHERE id = ?', args: [id] })
+    res.status(201).end()
+  } catch (error) {
+    console.error(error)
+    res.status(500).end()
+  }
+}
+)
+
+Router.get('/teapot', (req, res) => {
+  res.status(418).send("I'm a teapot")
 })
 
 // export the router
---
+++ backend/routes/mantenimiento/model.js
@@ -0,0 +1,78 @@
+import { createClient } from '@libsql/client'
+const Token = process.env.DB_MAN_TOKEN
+const urlMan = process.env.mode === 'production' ? process.env.DB_MAN_URL : 'file:./backend/local.db'
+
+const client = createClient({
+  authToken: Token,
+  url: urlMan
+})
+
+// initialize model of the database for the maintenance and machine
+
+// create the tables
+
+if (process.env.mode !== 'production') {
+  await client.execute(`
+  DROP TABLE IF EXISTS MAINTENANCE  
+`)
+  await client.execute(`
+ DROP TABLE IF EXISTS MACHINE
+`)
+}
+
+await client.execute(`
+  CREATE TABLE IF NOT EXISTS MACHINE (
+    id  INTEGER PRIMARY KEY AUTOINCREMENT,
+    type TEXT,
+    state TEXT,
+    availability int,
+    line INT
+  )
+`)
+
+await client.execute(`
+  CREATE TABLE IF NOT EXISTS MAINTENANCE (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    machineId INTEGER NOT NULL UNIQUE, 
+    type TEXT,
+    dateMaintenance  TEXT,
+    dateAvailability TEXT,
+    FOREIGN KEY (machineId) REFERENCES MACHINE(id)
+    )
+  `)
+
+// trigger to update the availability of the machine
+
+// preguntar a henry si se puede hacer un trigger para que cuando se inserte un mantenimiento se actualice la disponibilidad de la maquina
+await client.execute(`
+  CREATE TRIGGER IF NOT EXISTS update_availability
+  AFTER INSERT ON MAINTENANCE
+  BEGIN
+    UPDATE MACHINE
+    SET availability = 0
+    WHERE id = NEW.machineId;
+  END
+`)
+
+// trigger to update the availability of the machine if the maintenance is deleted
+await client.execute(`
+  CREATE TRIGGER IF NOT EXISTS delete_availability
+  AFTER DELETE ON MAINTENANCE
+  BEGIN
+    UPDATE MACHINE
+    SET availability = 1,
+    state = 'disponible'
+    WHERE id = OLD.machineId;
+  END
+`)
+
+// data for  development mode
+if (process.env.mode !== 'production') {
+  await client.execute({ sql: 'INSERT INTO MACHINE (type, state, availability, line) VALUES (?, ?, ?, ?)', args: ['chocolatera', 'uso', 'mantenimiento', 1] })
+  await client.execute({ sql: 'INSERT INTO MACHINE (type, state, availability, line) VALUES (?, ?, ?, ?)', args: ['caramelera', 'uso', 1, 1] })
+  await client.execute({ sql: 'INSERT INTO MACHINE (type, state, availability, line) VALUES (?, ?, ?, ?)', args: ['caramelizadora', 'uso', 1, 2] })
+
+  // add maintenance
+  await client.execute({ sql: 'INSERT INTO MAINTENANCE (machineId, type, dateMaintenance, dateAvailability) VALUES (?, ?, ?, ?)', args: [1, 'preventivo', '2024-02-20', '2024-02-22'] })
+}
+export default client
