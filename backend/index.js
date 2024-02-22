129204e902e18723aa360c7bbb42b6c1a39c0059
Luis <siul210704@gmail.com>
2024-02-21 23:15:55 -0400

    add more comments

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,14 @@ const app = express()
 
 // define port
 const port = process.env.PORT || 3000
+// parse the body of the request to object if the content type is application/json
+app.use(express.json())
 
 // Router api
 const ApiRouter = express.Router()
 
 // add the mantenimiento_router to the api router
 ApiRouter.use('/mantenimiento', matenimientoRouter)
-
 // add the api router to the app
 app.use('/api', ApiRouter)
 
