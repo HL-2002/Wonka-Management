2417b944f308b5bf1cbe5a14bfa426187586327f
Luis <siul210704@gmail.com>
2024-03-01 15:19:57 -0400

    add more  default data

--- backend/routes/mantenimiento/model.js
+++ backend/routes/mantenimiento/model.js
@@ -68,11 +68,14 @@ await client.execute(`
 
 // data for  development mode
 if (process.env.mode !== 'production') {
-  await client.execute({ sql: 'INSERT INTO MACHINE (type, state, availability, line) VALUES (?, ?, ?, ?)', args: ['chocolatera', 'uso', 'mantenimiento', 1] })
+  await client.execute({ sql: 'INSERT INTO MACHINE (type, state, availability, line) VALUES (?, ?, ?, ?)', args: ['chocolatera', 'uso', 0, 1] })
+  await client.execute({ sql: 'INSERT INTO MACHINE (type, state, availability, line) VALUES (?, ?, ?, ?)', args: ['chocolatera', 'disponible', 1, 1] })
+  await client.execute({ sql: 'INSERT INTO MACHINE (type, state, availability, line) VALUES (?, ?, ?, ?)', args: ['chocolatera', 'defectuosa', 0, 1] })
+
   await client.execute({ sql: 'INSERT INTO MACHINE (type, state, availability, line) VALUES (?, ?, ?, ?)', args: ['caramelera', 'uso', 1, 1] })
   await client.execute({ sql: 'INSERT INTO MACHINE (type, state, availability, line) VALUES (?, ?, ?, ?)', args: ['caramelizadora', 'uso', 1, 2] })
 
   // add maintenance
-  await client.execute({ sql: 'INSERT INTO MAINTENANCE (machineId, type, dateMaintenance, dateAvailability) VALUES (?, ?, ?, ?)', args: [1, 'preventivo', '2024-02-20', '2024-02-22'] })
+  await client.execute({ sql: 'INSERT INTO MAINTENANCE (machineId, type, dateMaintenance, dateAvailability) VALUES (?, ?, ?, ?)', args: [1, 'preventivo', '2024-03-05', '2024-03-22'] })
 }
 export default client
