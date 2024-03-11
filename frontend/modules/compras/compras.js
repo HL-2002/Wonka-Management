ab47d25c62a17ffef595d1e23371b224b4591467
angelddeleon <angeldeleonpr00@gmail.com>
2024-03-10 20:00:23 -0400

    Arregle el menu

--- frontend/modules/compras/compras.js
+++ frontend/modules/compras/compras.js
@@ -1,15 +1,42 @@
-//  MODAL
+document.addEventListener('DOMContentLoaded', function () {
+  //  CAMBIO DE SECCION
 
-const abrirModal = document.querySelector('.btn-open')
-const cerrarModal = document.querySelector('.salir')
-const modal = document.querySelector('.modal')
+  //  BOTONES DE MENU
+  const btnRegistro = document.querySelector('.btn-registro')
+  const btnRequisicion = document.querySelector('.btn-requisicion')
 
-abrirModal.addEventListener('click', (e) => {
-  e.preventDefault()
-  modal.classList.add('modal-show')
-})
+  //  SECCIONES DE REGISTRO Y REQUISICION
 
-cerrarModal.addEventListener('click', (e) => {
-  e.preventDefault()
-  modal.classList.remove('modal-show')
+  const solicitudesContenido = document.querySelector('.solicitudesContenido')
+  const registroContenido = document.querySelector('.registro')
+
+  btnRegistro.addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log('Hola')
+    document.querySelector('.registro').style.display = 'none'
+    document.querySelector('.solicitudesContenido').style.display = 'block'
+  })
+
+  btnRequisicion.addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log('Hola')
+    registroContenido.style.display = 'block'
+    solicitudesContenido.style.display = 'none'
+  })
+
+  //  MODAL
+
+  const abrirModal = document.querySelector('.btn-open')
+  const cerrarModal = document.querySelector('.salir')
+  const modal = document.querySelector('.modal')
+
+  abrirModal.addEventListener('click', (e) => {
+    e.preventDefault()
+    modal.classList.add('modal-show')
+  })
+
+  cerrarModal.addEventListener('click', (e) => {
+    e.preventDefault()
+    modal.classList.remove('modal-show')
+  })
 })
