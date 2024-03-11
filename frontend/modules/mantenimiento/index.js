44d92f274f193cdd0a9e9765052ab7067fbd1cd9
HL-2002 <henryslang8231682@gmail.com>
2024-03-11 14:24:49 -0400

    Finished machine display with amount

--- frontend/modules/mantenimiento/index.js
+++ frontend/modules/mantenimiento/index.js
@@ -23,8 +23,8 @@ function display_machines(machines) {
   // Crear colección de máquinas por tipo
   let choco = {
     uso: 0,
-    notificada: 0,
     disponible: 0,
+    notificada: 0,
     mantenimiento : 0,
     defectuosa: 0
   }
@@ -32,6 +32,10 @@ function display_machines(machines) {
   let cara = structuredClone(choco)
   let caramel = structuredClone(choco)
   let chicl = structuredClone(choco)
+
+  // Crear grid de números por tipo de máquina
+  let grid = []
+  let total = []
 
   // Añadir máquinas según su tipo
   machines.forEach((machine) => {
@@ -60,11 +64,72 @@ function display_machines(machines) {
     for (state in type) {
       container.innerHTML += `<div class='square ${state}'></div>`.repeat(type[state])
     }
+    // Añadir valores al grid
+    values = Object.values(type)
+    grid.push(values)
+    total.push(values.reduce((a, b) => a + b, 0))
   })
+
+  // Display de los números en orden
+  const data_grid = document.getElementById('grid')
+  // Trasponer grid para iterar por columnas y agregar total de máquinas por tipo
+  grid = grid[0].map((_, colIndex) => grid.map(row => row[colIndex]));
+  grid.push(total)
+  console.log(grid)
+
+  // Iterar para añadir datos al data_grid
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = -1; j < grid[i].length; j++) {
+      if (j == -1) {
+        switch (i) {
+          case 0:
+            data_grid.innerHTML += `<p style="color: #9f70fd;" class="first-item">En uso </p>`
+            break
+          case 1:
+            data_grid.innerHTML += `<p style="color: #42c087;" class="first-item">Disponibles </p>`
+            break
+          case 2:
+            data_grid.innerHTML += `<p style="color: #f17c37;" class="first-item">Notificadas</p>`
+            break
+          case 3:
+            data_grid.innerHTML += `<p style="color: #f1c40f;" class="first-item"> Mantenimiento</p>`
+            break
+          case 4:
+            data_grid.innerHTML += `<p style="color: #e74c3c;" class="first-item">Defectuosas</p>`
+            break
+          case 5:
+            data_grid.innerHTML += `<p style="color: #34495e;" class="first-item">Total</p>`
+            break
+        }
+      }
+      else {
+        data_grid.innerHTML += `<div class='grid-item'>${grid[i][j]}</div>`
+      }
+    }
+  }
 }
 
 function display_maintenance(machines) {
-  return null
+  // Obtener el contenedor de las máquinas en mantenimiento
+  const planificado = document.getElementById('planificado')
+  const realizando = document.getElementById('realizando')
+  let planificados = 0, realizandos = 0
+
+  // Añadir máquinas en mantenimiento
+  machines.forEach((machine) => {
+    // Validar tipo de mantenimiento y generar estado y mensaje
+    if (machine.state != "disponible") {
+      state = "correctivo"
+    }
+
+    if (machine.state === 'maintenance') {
+      const tag = `<div class="state-container">
+                      <div class="state ${state}">${machine.id}</div>
+                      <div class="state-info">Preventivo</div>
+                   </div>`
+      mantenimiento.innerHTML += tag
+    }
+  })
 }
 
 
