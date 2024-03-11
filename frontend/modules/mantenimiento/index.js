d683567e7b60b218cbdac873ad0db43a1c60036b
HL-2002 <henryslang8231682@gmail.com>
2024-03-11 13:29:17 -0400

    Added dynamic display of machine squares and added styling for machine maintenance

--- frontend/modules/mantenimiento/index.js
+++ frontend/modules/mantenimiento/index.js
@@ -1,3 +1,73 @@
+document.addEventListener('DOMContentLoaded', async () => { 
+  const machines = await getMachines().then((json)=>{return json.machines})
+  console.log(machines)
+  display_machines(machines)
+  display_maintenance(machines)
+
+})
+
+// Obtener el array de las máquinas
+async function getMachines() {
+  const response = await fetch('http://localhost:3000/api/mantenimiento/')
+  return response.json()
+}
+
+// Actualizar el display de las máquinas
+function display_machines(machines) {
+  // Obtener los contenedores de las máquinas
+  const chocolateras = document.getElementById('chocolateras')
+  const caramelizadoras = document.getElementById('caramelizadoras')
+  const carameleras = document.getElementById('carameleras')
+  const chicleras = document.getElementById('chicleras')
+
+  // Crear colección de máquinas por tipo
+  let choco = {
+    uso: 0,
+    notificada: 0,
+    disponible: 0,
+    mantenimiento : 0,
+    defectuosa: 0
+  }
+
+  let cara = structuredClone(choco)
+  let caramel = structuredClone(choco)
+  let chicl = structuredClone(choco)
+
+  // Añadir máquinas según su tipo
+  machines.forEach((machine) => {
+    switch (machine.type) {
+      case 'chocolatera':
+        choco[machine.state] += 1
+        break
+      case 'caramelizadora':
+        cara[machine.state] += 1
+        break
+      case 'caramelera':
+        caramel[machine.state] += 1
+        break
+      case 'chiclera':
+        chicl[machine.state] += 1
+        break
+    }
+  })
+
+  // Agrupar contenedores y tipos de máquinas para iterar
+  const col = [[chocolateras, choco], [caramelizadoras, cara], [carameleras, caramel], [chicleras, chicl]]
+
+  // Display de las máquinas en orden
+  col.forEach((set) => {
+    const [container, type] = set
+    for (state in type) {
+      container.innerHTML += `<div class='square ${state}'></div>`.repeat(type[state])
+    }
+  })
+}
+
+function display_maintenance(machines) {
+  return null
+}
+
+
 // obtener los botones de abrir y cerrar
 const btns = document.querySelectorAll('.btn-open')
 const btnsClose = document.querySelectorAll('.close-button')
