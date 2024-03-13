f1b213aeb3558a1ebf7daa6a7cee6bac3b5f1148
Angel De Leon <81979915+angelddeleon@users.noreply.github.com>
2024-03-12 21:08:55 -0400

    agregando la funcionalidad de proveedores dinamicamente,  solicitudes dinamicamente,

--- frontend/modules/compras/compras.js
+++ frontend/modules/compras/compras.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
   //  SECCIONES DE REGISTRO Y REQUISICION
 
   const solicitudesContenido = document.querySelector('.solicitudes')
-  const registroContenido = document.querySelector('.registro')
+  const registroContenido = document.querySelector('.registro-seccion')
 
   //  CAMBIAR SECCION DE REQUISICION A REGISTRO
 
@@ -44,5 +44,157 @@ document.addEventListener('DOMContentLoaded', function () {
   cerrarModal.addEventListener('click', (e) => {
     e.preventDefault()
     modal.classList.remove('modal-show')
+  })
+
+  //  CREAR REQUISICIONES DE FORMA DINAMICA
+
+  const seccionRequisiciones = document.querySelector('.solicitudes')
+
+  const requisiciones = [
+    {
+      nombre: 'Titulo',
+      categoria: 'Categoría A',
+      cantidad: '$100'
+    },
+    {
+      nombre: 'Titulo',
+      categoria: 'Categoría B',
+      cantidad: '$200'
+    },
+    {
+      nombre: 'Titulo',
+      categoria: 'Categoría C',
+      cantidad: '$300'
+    }
+  ]
+
+  requisiciones.forEach(requisicion => {
+    const divTarjeta = document.createElement('div')
+    divTarjeta.classList.add('solicitud')
+
+    const h2Nombre = document.createElement('h2')
+    h2Nombre.textContent = requisicion.nombre
+    divTarjeta.appendChild(h2Nombre)
+
+    const pCategoria = document.createElement('p')
+    pCategoria.textContent = `Categoría: ${requisicion.categoria}`
+    divTarjeta.appendChild(pCategoria)
+
+    const pCantidad = document.createElement('p')
+    pCantidad.textContent = `Cantidad: ${requisicion.cantidad}`
+    divTarjeta.appendChild(pCantidad)
+
+    const buttomRequisicion = document.createElement('a')
+    buttomRequisicion.setAttribute('href', '#')
+    buttomRequisicion.classList.add('btn-open')
+    buttomRequisicion.textContent = 'Efectuar'
+    buttomRequisicion.addEventListener('click', (e) => {
+      e.preventDefault()
+      modal.classList.add('modal-show')
+    })
+
+    divTarjeta.appendChild(buttomRequisicion)
+
+    seccionRequisiciones.appendChild(divTarjeta)
+  })
+
+  //  CREAR DIVS PROOVEDOR DINAMICO
+
+  const contenedor = document.querySelector('.seccion-provedores')
+
+  const tarjetas = [
+    {
+      nombre: 'Empresa 1',
+      categoria: 'Categoría A',
+      costo: '$100',
+      tardanza: '2 días'
+    },
+    {
+      nombre: 'Empresa 2',
+      categoria: 'Categoría B',
+      costo: '$200',
+      tardanza: '3 días'
+    },
+    {
+      nombre: 'Empresa 3',
+      categoria: 'Categoría C',
+      costo: '$300',
+      tardanza: '4 días'
+    }
+  ]
+
+  tarjetas.forEach(tarjeta => {
+    const divTarjeta = document.createElement('div')
+    divTarjeta.classList.add('tarjeta-proveedor')
+
+    const h2Nombre = document.createElement('h2')
+    h2Nombre.textContent = tarjeta.nombre
+    divTarjeta.appendChild(h2Nombre)
+
+    const pCategoria = document.createElement('p')
+    pCategoria.textContent = `Categoría: ${tarjeta.categoria}`
+    divTarjeta.appendChild(pCategoria)
+
+    const pCosto = document.createElement('p')
+    pCosto.textContent = `Costo: ${tarjeta.costo}`
+    divTarjeta.appendChild(pCosto)
+
+    const pTardanza = document.createElement('p')
+    pTardanza.textContent = `Tardanza en despachar: ${tarjeta.tardanza}`
+    divTarjeta.appendChild(pTardanza)
+
+    contenedor.appendChild(divTarjeta)
+  })
+
+  //  CREAR REGISTRO DINAMICO
+
+  const seccionRegistro = document.querySelector('.registro-seccion')
+
+  const registros = [
+    {
+      nombre: 'Titulo',
+      categoria: 'Categoría A',
+      cantidad: '$100'
+    },
+    {
+      nombre: 'Titulo',
+      categoria: 'Categoría B',
+      cantidad: '$200'
+    },
+    {
+      nombre: 'Titulo',
+      categoria: 'Categoría C',
+      cantidad: '$300'
+    }
+  ]
+
+  registros.forEach(registrosHechos => {
+    const divTarjeta = document.createElement('div')
+    divTarjeta.classList.add('registros-hechos')
+
+    const h2Nombre = document.createElement('h2')
+    h2Nombre.textContent = registrosHechos.nombre
+    divTarjeta.appendChild(h2Nombre)
+
+    const pCategoria = document.createElement('p')
+    pCategoria.textContent = `Categoría: ${registrosHechos.categoria}`
+    divTarjeta.appendChild(pCategoria)
+
+    const pCantidad = document.createElement('p')
+    pCantidad.textContent = `Cantidad: ${registrosHechos.cantidad}`
+    divTarjeta.appendChild(pCantidad)
+
+    const buttomRequisicion = document.createElement('a')
+    buttomRequisicion.setAttribute('href', '#')
+    buttomRequisicion.classList.add('btn-open')
+    buttomRequisicion.textContent = 'Ver'
+    buttomRequisicion.addEventListener('click', (e) => {
+      e.preventDefault()
+      modal.classList.add('modal-show')
+    })
+
+    divTarjeta.appendChild(buttomRequisicion)
+
+    seccionRegistro.appendChild(divTarjeta)
   })
 })
