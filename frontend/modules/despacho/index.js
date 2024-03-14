77e33d81192cbf00a3936bf3691ae8fb6398d41a
ElAdagioDeJP <129967733+ElAdagioDeJP@users.noreply.github.com>
2024-03-13 20:30:36 -0400

    Connect the sales API with the dispatch, you can see the user data in the console, you just need to take it and pass it as a table or several labels in the dispatch area

--- frontend/modules/despacho/index.js
+++ frontend/modules/despacho/index.js
@@ -9,3 +9,19 @@ function Verificar() {
     document.getElementById("errornofactura").style.display = "block";
   }
 }
+fetch('http://localhost:3000/api/ventas/orders')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Error en la solicitud');
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Hacer algo con los datos obtenidos
+    console.log(data);
+    const {products} = data;
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+console.log(products)
