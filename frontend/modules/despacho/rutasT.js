1b2d8df09e673f7d36e8508d33fb5cc19de79a2a
Usur Ryu <jearmando1@gmail.com>
2024-03-21 00:45:51 -0400

    Se crean todas las rutas de todos los pedidos,mas interactividad,se resolvieron problemas de clonacion You can see the routes with all the orders, and if you hover over each delivery point you can see which order it is.

--- frontend/modules/despacho/rutasT.js
+++ frontend/modules/despacho/rutasT.js
@@ -3,26 +3,56 @@ function autoResizeInput(input) {
     input.style.width = "auto";
     input.style.width = (input.scrollWidth + 10) + "px";
   }
-
+  
 
 document.addEventListener('DOMContentLoaded', async () => { 
   const address = localStorage.getItem('direccionPedido') + ' Carabobo Venezuela' 
+  
 //Pedir del Local
-console.log(address)
+// console.log(address)
 
 const url=`https://geocode.search.hereapi.com/v1/geocode?q=${address}&apikey=GOdm4EI-zu94iYyyCugZc0CJ1MUYqio36JoTAhF3b_c`
 
   // Reemplaza esto con tu URL
   const response = await fetch(url);
-  console.log('Antes del Response', response)
-  data = await response.json();
+    data = await response.json();
 
+    const addresses = await fetch('../../api/ventas/orders')
+    const dataAddresses = await addresses.json()
+    console.log('dataAddresses', dataAddresses)
+    let addressArray = []
+    dataAddresses.forEach((address) => {
+        addressArray.push(address.address) 
+    })
+    console.log('addressArray', address)
+    // console.log('dataAddresses', dataAddresses)
+    let waypoints = []
+    let totalLength = 0;
+    let totalTime = 0;
+    for(let address of addressArray) { 
+        const formattedAddress = address + ' Carabobo Venezuela'
+        const url = `https://geocode.search.hereapi.com/v1/geocode?q=${formattedAddress}&apikey=GOdm4EI-zu94iYyyCugZc0CJ1MUYqio36JoTAhF3b_c`
+        const response = await fetch(url)
+        const data = await response.json()
+        console.log(data)
 
-  
+            //Peticion de origen a cada dirección obteniendo coordenadas de data.items[0].position
+            // sumar el tiempo y la distancia por cada iteracción
+            waypoints.push(data.items[0].position)
+        const { lat, lng } = data.items[0].position
+        const routes = await fetch(`https://router.hereapi.com/v8/routes?transportMode=truck&origin=10.2363953,-67.9649982&destination=${lat},${lng}&return=summary&apikey=GOdm4EI-zu94iYyyCugZc0CJ1MUYqio36JoTAhF3b_c`)
+        const routesData = await routes.json()
+        const { length, duration } = routesData.routes[0].sections[0].summary
+        totalLength += length;
+        totalTime += duration;
+    }
+    console.log('coordinates', waypoints)
+    console.log('totalLenght', totalLength)
+    console.log('totalTime',totalTime)
   console.log('locationData', data.items[0])
   const { lat, lng } = data.items[0].position
-  console.log('lat', lat)
-  console.log('lng', lng)
+//   console.log('lat', lat)
+//   console.log('lng', lng)
 
 const route = await fetch(`https://router.hereapi.com/v8/routes?transportMode=truck&origin=10.2363953,-67.9649982&destination=${lat},${lng}&return=summary&apikey=GOdm4EI-zu94iYyyCugZc0CJ1MUYqio36JoTAhF3b_c`)
 const dataRoute = await route.json()
@@ -31,9 +61,11 @@ const dataRoute = await route.json()
   console.log('duration', duration)
   console.log('dataRoute', dataRoute)
   const distance = document.getElementById('destino')
-  const municipio = document.getElementById('municipio')
-  km = length / 1000
-  tiempo = duration / 60
+    const municipio = document.getElementById('municipio')
+    totalLength += length
+    totalTime += duration
+  km = totalLength / 1000
+  tiempo = totalTime / 60
   distance.value = parseFloat(km.toFixed(1));
   municipio.value = parseFloat(tiempo.toFixed(1));
   // Initiate and authenticate your connection to the HERE platform:
@@ -63,7 +95,10 @@ const origin = { lat: 10.2363953, lng: -67.9649982 }
 const destination = { lat, lng}
 
 const ui = H.ui.UI.createDefault(map,defaultLayers);
+// const zoomControl = new H.ui.ZoomControl();
+// ui.addControl('Zoom', zoomControl);
 
+    const waypointMarkers = [];
 
 // Create the parameters for the routing request:
 const routingParameters = {
@@ -75,7 +110,12 @@ const routingParameters = {
     destination: `${destination.lat},${destination.lng}`,
     // Include the route shape in the response
     return: 'polyline',
-}
+    // Add a via parameter to the query for each coordinate pair:
+    'via': new H.service.Url.MultiValueQueryParameter(
+        waypoints.map(wp => `${wp.lat},${wp.lng}`)
+    )
+};
+
 
 // Define a callback function to process the routing response:
 const onResult = function (result) {
@@ -88,14 +128,14 @@ const onResult = function (result) {
                 H.geo.LineString.fromFlexiblePolyline(section.polyline)
             )
         })
-
+        
         // Create an instance of H.geo.MultiLineString
         const multiLineString = new H.geo.MultiLineString(lineStrings)
 
         // Create a polyline to display the route:
         const routeLine = new H.map.Polyline(multiLineString, {
             style: {
-                strokeColor: 'blue',
+                strokeColor: '#7B74E0',
                 lineWidth: 3,
             },
         })
@@ -106,9 +146,38 @@ const onResult = function (result) {
 
 // Crear un icono personalizado para el destino:
         const destinationIcon = new H.map.Icon('<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="10" r="3"/><path d="M12 22s-8-4.5-8-9.8A8 8 0 0 1 12 2a8 8 0 0 1 8 8c0 5.3-8 9.8-8 9.8z"/></svg>');
+        const waypointIcon = new H.map.Icon('<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="blue" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="10" r="3"/><path d="M12 22s-8-4.5-8-9.8A8 8 0 0 1 12 2a8 8 0 0 1 8 8c0 5.3-8 9.8-8 9.8z"/></svg>');
         const endMarker = new H.map.Marker(destination, { icon: destinationIcon });
-        
+        waypoints.forEach((waypoint, index) => {
+            const waypointMarker = new H.map.Marker({
+                lat: waypoint.lat,
+                lng: waypoint.lng
+            }, {icon:waypointIcon});
+            // Populate the waypointMarkers array:
+            waypointMarkers.push(waypointMarker);
+            const waypointBubble = new H.ui.InfoBubble({
+                lat: waypoint.lat,
+                lng: waypoint.lng
+            }, {
+                content: 'Este punto es de la orden ' + dataAddresses[index].orderId
+            });
 
+            // Add the InfoBubble to the UI:
+            ui.addBubble(waypointBubble);
+
+            // Close the InfoBubble initially:
+            waypointBubble.close();
+
+            // Show the InfoBubble when the cursor enters the marker:
+            waypointMarker.addEventListener('pointerenter', function () {
+                waypointBubble.open();
+            });
+
+            // Hide the InfoBubble when the cursor leaves the marker:
+            waypointMarker.addEventListener('pointerleave', function () {
+                waypointBubble.close();
+            });
+        });
         const originBubble = new H.ui.InfoBubble(origin, {
             content: 'Este es el origen de la ruta'
           });
@@ -131,11 +200,11 @@ const onResult = function (result) {
         
 
         
-
+        console.log('waypointMarkers', waypointMarkers)
         // Create a H.map.Group to hold all the map objects and enable us to obtain
         // the bounding box that contains all its objects within
         const group = new H.map.Group()
-        group.addObjects([routeLine, startMarker, endMarker])
+        group.addObjects([routeLine, startMarker, endMarker, ...waypointMarkers]);
         // Add the group to the map
         map.addObject(group)
 
